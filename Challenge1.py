c19e92fd9880a514999983d1d6a2654a69c1a2fd
Mohammad Bayu Rizki <widjanarko020@gmail.com>
2025-02-15 18:25:14 +0700

    update source code

--- Challenge1.py
+++ Challenge1.py
@@ -1,33 +1,52 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
-list_temporary = []
+list_temporary = [
+    {"temperature": 25, "humidity": 60, "timestamp": "2023-10-10T12:00:00"},
+    {"temperature": 26, "humidity": 58, "timestamp": "2023-10-10T12:05:00"},
+    {"temperature": 24, "humidity": 65, "timestamp": "2023-10-10T12:10:00"}
+]
 
-@app.route('/sensor_farm', methods = ['POST'])
-def simpan_data_sensorfarm ():
+@app.route('/sensor_farm', methods=['POST', 'GET'])
+def simpan_data_sensorfarm():
     if request.method == 'POST':
         body = request.get_json()
-        temperature = body['temperature']
-        humidity = body['humidity']
-        timestamp = body['timestamp']
-        
-        average_data_sensor = (temperature + humidity)/2
+        temperature = body.get('temperature')
+        humidity = body.get('humidity')
+        timestamp = body.get('timestamp')
+
+        average_data = (temperature + humidity) / 2
         
         list_temporary.append({
-            "temperature":temperature,
-            "\nhumidity":humidity,
-            "\ntimestamp":timestamp,
-            "\nAverage":average_data_sensor
+            "temperature": temperature,
+            "humidity": humidity,
+            "timestamp": timestamp,
+            "Average": average_data
         })
-        print("Current Database: ",list_temporary)
-        return {
-            "message":"Hello, I have your request",
-            "average current": average_data_sensor
-        }
-    elif not request.method:
-        return {
-            "message":"request kamu gagal, mohon maaf"
-        }
+        
+        return jsonify({
+            "message": "Data berhasil disimpan",
+            "average_current": average_data
+        })
+    
+    elif request.method == 'GET':
+        return jsonify(list_temporary)
+
+@app.route('/sensor1/temperature/avg', methods=['GET'])
+def get_avg_temperature():
+    if not list_temporary:
+        return jsonify({
+            "message": "Tidak ada data suhu tersedia",
+            "average_temperature": None
+        }), 200
+
+    total = sum(entry['temperature'] for entry in list_temporary)
+    average_temp = total / len(list_temporary)
+    
+    return jsonify({
+        "average_temperature": round(average_temp, 2)  # data dibulatkan
+    })
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+    app.run(port=5000, debug=True)
